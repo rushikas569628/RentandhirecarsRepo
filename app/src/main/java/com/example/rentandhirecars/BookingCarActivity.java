0114c38032cc3be8a405cfabd944116b656d61ea
Vinil Kappa ChandraBabu <156454928+vinil2405@users.noreply.github.com>
2024-10-11 21:47:37 -0500

    Added Layouts for Booking car and details of car

---
+++ app/src/main/java/com/example/rentandhirecars/BookingCarActivity.java
@@ -0,0 +1,20 @@
+package com.example.rentandhirecars;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class BookingCarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_booking_car);
+
+    }
+}
